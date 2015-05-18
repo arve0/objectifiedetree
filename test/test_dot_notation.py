3fd98812b8b4cc07f616dadb45fed0e443489e09
arve0 <arve.seljebu@gmail.com>
2015-05-18 11:57:32 +0200

    test element.attrib

--- test/test_dot_notation.py
+++ test/test_dot_notation.py
@@ -6,7 +6,8 @@ from objectifiedetree import *
 
 
 def test_dot_notation():
-    tree = ET.fromstring('<root><a><b/></a></root>')
+    tree = ET.fromstring('<root><a><b attr1="asdf"/></a></root>')
 
     assert tree.a is not None
     assert tree.a.b is not None
+    assert tree.a.b.attrib['attr1'] == "asdf"
